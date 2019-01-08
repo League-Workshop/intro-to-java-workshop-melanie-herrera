279124ca7c4b77ddfd9a11367b579ba7c0665733
melanie-herrera <46504362+melanie-herrera@users.noreply.github.com>
2019-01-08 14:57:06 -0800

    Day2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,33 +1,41 @@
 package section2;
 
 import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot melanie=new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+melanie.setSpeed(100);
 		// 5. Set the pen width to 5
-
+melanie.penDown();
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for(int i=0;i<4;i++) { 
 			// 7. Set the pen color to random
-	
+melanie.setPenWidth(5);
 			// 1. Call the drawSquare() method
-	
+drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
-	}
+melanie.turn(90);
+	}}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		melanie.move(100);
+		melanie.turn(90);
+		melanie.move(100);
+		melanie.turn(90);
+		melanie.move(100);
+		melanie.turn(90);
+		melanie.move(100);
+		melanie.turn(90);
 	}
 
 	public static void main(String[] args) {
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,23 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot melanie =new Robot(); 
 		// 5. Set your robot's pen down 
-		
+		melanie .penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		melanie.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i=0;i<75;i++) {
 			// 7. Change the pen color to random
-		
+		melanie.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			melanie.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
 		
+	    melanie.turn(360/7);
+	
 			// 8. Set the pen width to i
-			
+			melanie.setPenWidth(i);
+		}
 	}
 }
