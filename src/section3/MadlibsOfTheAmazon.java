c56652eef0686fdda86fd33f609bf6c3c60c5253
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-11 13:52:47 -0800

    madlibsof the amazon

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -11,16 +11,22 @@ public class MadlibsOfTheAmazon {
 		// Get the user to enter an adjective
 String adjective=JOptionPane.showInputDialog("spell a adective");
 		// Get the user to enter a type of liquid
-String answer=JOptionPane.showInputDialog("write a type of liquid");
+String Liquid=JOptionPane.showInputDialog("write a type of liquid");
 		// Get the user to enter a body part
-JOptionPane.showInputDialog("type in a body part");
+String bodypart=JOptionPane.showInputDialog("type in a body part");
 		// Get the user to enter a verb
-JOptionPane.showInputDialog("type a verb")
+String verb=JOptionPane.showInputDialog("type a verb");
 		// Get the user to enter a place
-JOptionPane.showInputDialog("type in a place");
+String place=JOptionPane.showInputDialog("type in a place");
 		// Fit the user's words into this sentence, and save it in a String:
+String sentence = "Piranhas are more "+adjective+" during the day, so cross the river at\n" + 
+		" night. Piranhas are attracted to fresh type of "+Liquid+" and will most\n" + 
+		" likely take a bite out of your body part if you "+verb+" Whatever\n" + 
+		" you do, if you have an open wound, try to find another way to get to a "+place+"\n"; 
+JOptionPane.showMessageDialog(null,sentence);
+//		"		// back to the "+place+". Good luck!
 		// Piranhas are more [adjective] during the day, so cross the river at
-		// night. Piranhas are attracted to fresh [type of liquid] and will most
+ // night. Piranhas are attracted to fresh [type of liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
